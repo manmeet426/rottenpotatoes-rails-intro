116b93f886f0ed871bd503966163d3f26b1d5b31
EC2 Default User <ec2-user@ip-172-31-38-175.us-east-2.compute.internal>
2018-09-28 02:34:42 +0000

    PART 3 COMPLETE.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,28 +11,66 @@ class MoviesController < ApplicationController
   end
 
   def index
+    session[:checked_ratings] ||= Hash.new
+    session[:selected_order] ||= ""
+    
+    if(params.size == 2)
+      params[:sort_by] ||= session[:selected_order]
+      params[:ratings] ||= session[:checked_ratings]
+      redirect_to movies_path(params) and return
+    end  
     #@movies = Movie.all
     #if params[:sort_by]== "release_date"
     #  @movies = Movie.order(:release_date)
     #elsif params[:sort_by] == "title"
     #  @movies = Movie.order(:title)
     #end
+    
     @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    if params[:ratings]
-      @checked_ratings = params[:ratings].keys
-    else
-      @checked_ratings = @all_ratings
+    
+    if !params[:ratings].nil?
+      session[:checked_ratings] = params[:ratings]
+    elsif !params[:commit].nil?
+      session[:checked_ratings] = @all_ratings
+    end  
+    
+   
+    
+    #if params[:ratings]
+    #  session[:checked_ratings] = params[:ratings]      #  .keys
+    #else
+    #  session[:checked_ratings] = @all_ratings           # Hash.new
+    #end
+    
+    #if params[:ratings]
+    #  @checked_ratings = params[:ratings].keys
+    #else
+    #  @checked_ratings = @all_ratings
+    #end
+    
+    @checked_ratings = session[:checked_ratings]
+    
+    if !params[:sort_by].nil?
+      session[:selected_order] = params[:sort_by]
+    end 
+    
+    sort_by = (session[:selected_order] == "")?"title":session[:selected_order]
+    
+    @movies = Movie.order("#{sort_by}")
+    
+    if(@checked_ratings.any?)              #.keys
+      @movies = @movies.where(:rating => @checked_ratings.keys)  
     end
     
-    @checked_ratings.each do |rating|
-      params[rating] = true
-    end  
+    #@checked_ratings.each do |rating|
+    #  params[rating] = true
+    #end  
     
-    if params[:sort_by]
-      @movies = Movie.order(params[:sort_by])
-    else
-      @movies = Movie.where(:rating => @checked_ratings)
-    end  
+    #if params[:sort_by]
+    #  @movies = Movie.order(params[:sort_by])
+    #else
+    #  @movies = Movie.where(:rating => @checked_ratings)
+    #end  
   end
 
   def new
